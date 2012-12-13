9c3183c4bf418c337d5e1d92e0a330b41813b90c
Neomac <antoine18@gmail.com>
2012-12-13 08:32:25 +0100

    Mise en place fichier de base

--- LO44/LO44/main.c
+++ LO44/LO44/main.c
@@ -2,18 +2,43 @@
 //  main.c
 //  LO44
 //
-//  Created by Antoine Galluet on 13/12/12.
+//  Created by Antoine Galluet on 12/12/12.
 //  Copyright (c) 2012 Antoine Galluet. All rights reserved.
 //
 
 #include <stdio.h>
+#include </Users/HP-Touchpad/projetperso_GI01/LO44/LO44/ListeBit.h>
 
-int main(int argc, const char * argv[])
-{
-
-    // insert code here...
-    printf("Vous etes nul\n");
-    printf("Vous etes nul!!!!!!!\n");
-    return 0;
+int main (){
+    
+    printf("Salut tout le monde !!!\n");
+    int i, Z;
+    ListeBit Indiv = NULL;
+    for (i=1; i<11; i++){
+        Indiv = ajouter_queue(Indiv,1);
+    }
+    Z=compt(Indiv);
+    printf(Z);
+    
 }
 
+
+
+int compt(ListeBit A){
+    int n = 0;
+    if (A->valeur==0){
+        return(0);
+    }
+    else if (A->valeur==1){
+        return(1);
+    }
+    else{
+        if (A->valeur%2==1){
+            return(2^(n+1)+ compt((reste(A))));
+            n++;
+        }
+        else{
+            return(compt(reste(A)));
+        }
+    }
+}
---
+++ LO44_Bon/LO44_Bon/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  LO44_Bon
+//
+//  Created by Antoine Galluet on 13/12/12.
+//  Copyright (c) 2012 Antoine Galluet. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
