92edd09d679c4bf792e4ac72d2f4cab32331e045
toptere <arnaudv@mac.com>
2012-12-13 10:53:56 +0100

    Fail

--- LO44/LO44/main.c
+++ LO44/LO44/main.c
@@ -1,28 +1,54 @@
 //
-//  main.c
-//  LO44
+//	main.c
+//	LO44
 //
-//  Created by Antoine Galluet on 12/12/12.
-//  Copyright (c) 2012 Antoine Galluet. All rights reserved.
+//	Created by Antoine Galluet on 12/12/12.
+//	Copyright (c) 2012 Antoine Galluet. All rights reserved.
 //
 
 #include <stdio.h>
-#include </Users/HP-Touchpad/projetperso_GI01/LO44/LO44/listebit.c>
-#include </Users/HP-Touchpad/projetperso_GI01/LO44/LO44/random1.c>
+#include <stdlib.h>
+#include <math.h>
+//#include </Users/HP-Touchpad/projetperso_GI01/LO44/LO44/listebit.c>
+//#include </Users/HP-Touchpad/projetperso_GI01/LO44/LO44/random1.c>
+//#include </users/arnaudv/documents/travail/utbm/lo44/lo44_projet/projetperso_GI01/LO44/LO44/random1.c>
+//#include </users/arnaudv/documents/travail/utbm/lo44/lo44_projet/projetperso_GI01/LO44/LO44/listebit.c>
+
+
+typedef struct elem{
+    int valeur;
+    struct elem *suivant;
+}Bit;
+
+typedef Bit *ListeBit;
+
 
 int main (){
-    
-    printf("Salut tout le monde !!!\n");
-    printf("Top est trop fort !!!\n");
-    int i, Z;
-    ListeBit Indiv = NULL;
-    for (i=1; i<11; i++){
-        Indiv = ajouter_queue(Indiv,1);
-    }
-    Z=compt(Indiv);
-    printf(Z);
-    random1(20);
-    
+		
+		/*printf("Salut tout le monde !!!\n");
+		printf("Top est trop fort !!!\n");*/
+		
+		printf("Test 1\n");
+		
+		int i, Z;
+		
+		printf("Test 2\n");
+		
+		ListeBit Indiv = NULL;
+		
+		printf("Test 3\n");
+		
+		for (i=1; i<11; i++){
+			
+			printf("Test 1%d\n", i);
+			
+			Indiv = ajouter_queue(Indiv,1);
+			
+		}
+		Z=compt(Indiv);
+		printf("Z = %d\n\n", Z);
+		//random1(20);
+		
 }
 
 
@@ -47,4 +73,24 @@ int compt(ListeBit A){
             n++;
         }
     }
+}
+
+ListeBit ajouter_queue(ListeBit A, int v){
+    ListeBit T = A;
+    while (T->suivant!=NULL){
+        T=T->suivant;
+    }
+    ListeBit new = (ListeBit*) malloc (sizeof (ListeBit));
+    new -> valeur = v;
+    new -> suivant = NULL;
+    return(T->suivant = new);
+}
+
+ListeBit creer(){
+    ListeBit A = NULL;
+    return(A);
+}
+
+ListeBit reste(ListeBit A){
+    return(A=A->suivant);
 }
--- LO44/LO44/random1.c
+++ LO44/LO44/random1.c
@@ -2,15 +2,15 @@
 #include<stdlib.h>
 #include<time.h>
 
-void main (void)
+/*void main (void)
 {
 	int nbv, i, prob, result; 
 	i=0;
 	
-	printf("\n\nRentrer le nombre de valeurs à générer:\n\n");
+	printf("\n\nRentrer le nombre de valeurs a generer:\n\n");
 	scanf("%d", &nbv);
 	
-	printf("\n\nRentrer en pourcentage la probabilité de 1 à générer:\n\n");
+	printf("\n\nRentrer en pourcentage la probabilité de 1 a generer:\n\n");
 	scanf("%d", &prob);
 	
 	printf("\n\n");
@@ -22,22 +22,21 @@ void main (void)
 	
 	printf("\n\n");
 	
-}
+}*/
 
 int random1(int prob)
 
 // prob est un entier compris entre 0 et 100
-// Le sous-programme retournera aléatoirement 1 selon la probabilité choisie, sinon il retournera 0
+// Le sous-programme retournera aleatoirement 1 selon la probabilité choisie, sinon il retournera 0
 
 {
 	srand(time(NULL) + rand()); //Initialise aléatoirement la fonction rand()
 	
-	if ( (rand() % 100 + 1) <= prob ){ 	//Tire aléatoirement un entier entre 1 et 100 et le compare à prob
-		return 1;													//Si c'est inférieure il renvoie 1
+	if ( (rand() % 100 + 1) <= prob ){ 	//Tire aleatoirement un entier entre 1 et 100 et le compare a prob
+		return 1;													//Si c'est inferieure il renvoie 1
 	}
 	else{
 		return 0;													//Sinon il renvoie 0
 	}
 }
 
-s
